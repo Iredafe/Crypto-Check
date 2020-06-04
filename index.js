e3cf23386ffd3709d6d2117c0ff61b1839d3a700
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-06-04 16:17:27 -0700

    create api call

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require ("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -7,6 +8,16 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 })
+
+app.post("/", function(req, res){
+//console.log(req.body.crypto);
+
+request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD",
+function(error, response, body){
+console.log(response.body);
+});
+
+});
 
 
 app.listen(3000, function(){
