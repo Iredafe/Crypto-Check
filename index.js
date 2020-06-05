a3b0019d035f23a96a3d41aefceed595920d8435
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-06-05 14:10:03 -0700

    render the requests of each crypto and fiat currency

--- index.js
+++ index.js
@@ -12,9 +12,23 @@ app.get("/", function(req, res){
 app.post("/", function(req, res){
 //console.log(req.body.crypto);
 
-request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD",
-function(error, response, body){
-console.log(response.body);
+var crypto = req.body.crypto;
+var fiat = req.body.fiat;
+
+var baseURL="https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+var finalURL = baseURL+crypto+fiat;
+
+request(finalURL, function(error, response, body){
+
+//console.log(response.body);
+var data = JSON.parse(body);
+var price = data.last;
+var currentDate = data.display_timestamp;
+
+res.write("The current date is " +currentDate);
+res.write("<h1>The current price of" + crypto +"is " + price + " "+ fiat +"<h1/>");
+res.send();
+
 });
 
 });
